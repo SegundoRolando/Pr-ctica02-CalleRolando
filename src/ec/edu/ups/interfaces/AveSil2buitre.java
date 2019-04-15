90d9770a8f39a783388b58ba9f40011f2f323e84
ASUS <ASUS@172.16.209.31>
2019-04-14 20:21:06 -0500

    ....

---
+++ src/ec/edu/ups/interfaces/AveSil2buitre.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.interfaces;
+import ec.edu.ups.clases.Acciones;
+/**
+ *
+ * @author ASUS
+ */
+public abstract class AveSil2buitre extends Ave implements Acciones {
+
+    @Override
+    public void comersemillas() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void dormir() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void moverse() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void volar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void reproducirse() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
